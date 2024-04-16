0e7f64b6fe9fc922a02fa338d6042bdde5e6dd2d
A00272016 <156268503+A00272016@users.noreply.github.com>
2024-04-16 23:56:56 +0000

    Step 2 for assignment is done

--- GeometryApplication_CosoleApp/Program.cs
+++ GeometryApplication_CosoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+var featureManagement = new Dictionary<string, string> {
+    { "FeatureManagement:Square", "true" },
+    { "FeatureManagement:Rectangle", "false" },
+    { "FeatureManagement:Triangle", "true" }
+};
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection(featureManagement)
+    .Build();
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is my Geometry Application Assignment
 Steps to Complete this assignment:
 1.I have made class library test library and console app for the assignment.
 2.Added Some Classes in Class Library
+3.Step 2 for assignment is done
