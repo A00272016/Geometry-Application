677fd090a7d3e60ea39d857ef776e9d799c3f023
A00272016 <156268503+A00272016@users.noreply.github.com>
2024-04-17 06:09:15 +0000

    Added Test Cases For Square.

--- GeometryApplication_Test/UnitTest1.cs
+++ GeometryApplication_Test/UnitTest1.cs
@@ -1,10 +1,68 @@
 namespace GeometryApplication_Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
-public class UnitTest1
+public class SquareTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestCalculateArea()
     {
+        double sideLength = 5;
+        Square square = new Square(sideLength);
+        double area = square.CalculateArea();
+        Assert.AreEqual(25, area);
     }
+
+    [TestMethod]
+    public void TestCalculatePerimeter()
+    {
+        double sideLength = 5;
+        Square square = new Square(sideLength);
+        double perimeter = square.CalculatePerimeter();
+        Assert.AreEqual(20, perimeter);
+    }
+    
+}
+[TestClass]
+public class SquareTests2
+{
+    [TestMethod]
+    public void TestCalculateArea()
+    {
+        double sideLength = 90;
+        Square square = new Square(sideLength);
+        double area = square.CalculateArea();
+        Assert.AreEqual(50, area);
+    }
+
+    [TestMethod]
+    public void TestCalculatePerimeter()
+    {
+        double sideLength = 5;
+        Square square = new Square(sideLength);
+        double perimeter = square.CalculatePerimeter();
+        Assert.AreEqual(90, perimeter);
+    }
+[TestClass]
+public class SquareTests3
+{
+    [TestMethod]
+    public void TestCalculateArea()
+    {
+        double sideLength = 6;
+        Square square = new Square(sideLength);
+        double area = square.CalculateArea();
+        Assert.AreEqual(80, area);
+    }
+
+    [TestMethod]
+    public void TestCalculatePerimeter()
+    {
+        double sideLength = 10;
+        Square square = new Square(sideLength);
+        double perimeter = square.CalculatePerimeter();
+        Assert.AreEqual(100, perimeter);
+    }
+    
+}
 }
--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Steps to Complete this assignment:
 4.Solve Some Errors which I was facing in the code.
 5.need to add test cases.
 6.Made some Changes in the code get the proper output according to the instructions.
-7.Solved Error which was facing in triangle
+7.Solved Error which was facing in triangle.
+8.Added Test Cases For Square.
