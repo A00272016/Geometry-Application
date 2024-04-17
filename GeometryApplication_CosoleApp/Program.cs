1e5596f16cc2410b0c9a70d1ccd5a59ff569e9d2
A00272016 <156268503+A00272016@users.noreply.github.com>
2024-04-17 05:01:30 +0000

    additnal work

--- GeometryApplication_CosoleApp/Program.cs
+++ GeometryApplication_CosoleApp/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FeatureManagement;
 
-var featureManagement = new Dictionary<string, string> {{ "FeatureManagement:Square", "false"}, { "FeatureManagement:Rectangle", "true"}, { "FeatureManagement:Triangle", "true"}};
+var featureManagement = new Dictionary<string, string> {{ "FeatureManagement:Square", "false"}, { "FeatureManagement:Rectangle", "false"}, { "FeatureManagement:Triangle", "true"}};
 
 IConfigurationRoot configuration = new ConfigurationBuilder().AddInMemoryCollection(featureManagement).Build();
 var services = new ServiceCollection();
@@ -30,4 +30,16 @@ Rectangle rectangle = new Rectangle(length,width);
 
 Console.WriteLine($"Area of the Square: {rectangle.CalculateArea()}");
 Console.WriteLine($"Perimeter of the Square: {rectangle.CalculatePerimeter()}");
+}
+if (await featureManager.IsEnabledAsync("Triangle"))
+{
+Console.WriteLine("Enter the side length of the triangle:");
+double side1 = Convert.ToDouble(Console.ReadLine());
+double side2 = Convert.ToDouble(Console.ReadLine());
+double side3 = Convert.ToDouble(Console.ReadLine());
+
+Rectangle rectangle = new Rectangle(side1,side2,side3);
+
+Console.WriteLine($"Area of the triangle: {triangle.CalculateArea()}");
+Console.WriteLine($"Perimeter of the triangle: {triangle.CalculatePerimeter()}");
 }
